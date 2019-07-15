60cd4513137220c37aaa75ebdb77c9c4418f528a
1TENCLAUDIO <1tenclaudio@gmail.com>
2019-07-15 16:35:06 -0300

    adicionando script e modificando o readname

---
+++ R/iniciais.R
@@ -0,0 +1,16 @@
+ls()
+#para listar objetos na area de trabalho
+rm(list())
+#para remover objetos da area de trabalho
+getwd()
+#qual e a pasta de trabalho?
+CTRL ENTER
+#inserir script no console
+git status
+#estado da arte
+git add
+#adicionar objeto
+git commit - m
+#adicionar mensagem readme.txt localmente
+git push - u origin master
+#enviar a nuvem do github
